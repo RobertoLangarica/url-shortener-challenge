d72b2da6f67308a8374772af974baec63043b430
Roberto <roberto@villavanilla.com>
2017-12-19 20:24:57 -0600

    - Added to the package.json a start script - Modified the function generateHash in  url.js to generate a shortened hash instead of using the uuid as return value. - Modified the function shorten in url.js to accept only the parameter url and generate inside the hash for the shortened url instead of accepting the url already hashed. - Modified the function shorten in url.js to avoid the shortening of an already shortened url.

--- app/url/routes.js
+++ app/url/routes.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const url = require('./url');
 
-
 router.get('/:hash', async (req, res, next) => {
-
-  const source = await url.getUrl(req.params.hash);
+  const source = await url.getUrl({hash:req.params.hash});
 
   // TODO: Respond accordingly when the hash wasn't found (404 maybe?)
 
@@ -30,13 +28,12 @@ router.get('/:hash', async (req, res, next) => {
   }
 });
 
-
 router.post('/', async (req, res, next) => {
 
   // TODO: Validate 'req.body.url' presence
 
   try {
-    let shortUrl = await url.shorten(req.body.url, url.generateHash(req.body.url));
+    let shortUrl = await url.shorten(req.body.url);
     res.json(shortUrl);
   } catch (e) {
     // TODO: Personalized Error Messages
@@ -45,7 +42,7 @@ router.post('/', async (req, res, next) => {
 });
 
 
-router.delete('/:hash/:removeToken', async (req, res, next) => {
+router.delete('/:hash/remove/:removeToken', async (req, res, next) => {
   // TODO: Remove shortened URL if the remove token and the hash match
   let notImplemented = new Error('Not Implemented');
   notImplemented.status = 501;
--- app/url/url.js
+++ app/url/url.js
@@ -7,28 +7,102 @@ const parseUrl = require('url').parse;
 const validUrl = require('valid-url');
 
 /**
- * Lookup for existant, active shortened URLs by hash.
+ * Lookup for existant, active shortened URLs by the query provided.
  * 'null' will be returned when no matches were found.
- * @param {string} hash
+ * @param {string} query any json object for the query for example a hash to lookup by hash{"hash":"value"}
  * @returns {object}
  */
-async function getUrl(hash) {
-  let source = await UrlModel.findOne({ active: true, hash });
+async function getUrl(query) {
+  let source = await UrlModel.findOne({ active: true, ...query });
   return source;
 }
 
 /**
  * Generate an unique hash-ish- for an URL.
- * TODO: Deprecated the use of UUIDs.
- * TODO: Implement a shortening algorithm
  * @param {string} id
  * @returns {string} hash
  */
 function generateHash(url) {
-  // return uuidv5(url, uuidv5.URL);
-  return uuidv4();
+  const  uuid = uuidv4();
+
+  // lets convert the uuid to a decimal value
+  const uuidDecimal = convertUUIDToDecimal(uuid);
+
+  // converting the decimal to a hash string
+  return generateHashFromDecimal(uuidDecimal);
 }
 
+/**
+ * Convert the provided uuid to a decimal value
+ * @returns {int} a decimal integer value 
+ */
+function convertUUIDToDecimal(uuid){
+  // split in words (8-4-4-4-12)
+  const s8   = uuid.substring(0,8);
+  const s4_1 = uuid.substring(9,13);
+  const s4_2 = uuid.substring(14,18);
+  const s4_3 = uuid.substring(19,23);
+  const s12  = uuid.substring(24,36);
+
+
+  let uuidDecimal = 0;
+
+  // first s8 half as the initial value, next s8 half subdivied in two and added with himself to be used as multiplier for the initial value
+  // Max posibble value = 0xFFFF*0xFE01 = 0xFE0001FF
+  uuidDecimal += parseInt(s8.substring(0,4), 16);
+  uuidDecimal *= (parseInt(s8.substring(4,6), 16) * parseInt(s8.substring(6,8), 16));
+
+  // adding the s4 first characters to use the result as a multiplier for the previous value
+  //max value = 0xFE0001FF * 0xF0 = 0xEE2001DF10
+  uuidDecimal *= (parseInt(s4_1.substring(0,1), 16) + parseInt(s4_2.substring(0,1), 16) + parseInt(s4_3.substring(0,1), 16) + parseInt(s4_1.substring(1,2), 16))<<2;
+
+  // Helper function that takes every character and multiply that character with an initial value (every new multiplication is added to the result)
+  const multiplyString = (target)=>{
+    let result = 1;
+    let characterValue;
+    for(let i = 0; i < target.length; i++){
+      //avoid zero multiplication
+      characterValue = parseInt(target.charAt(i),16);
+      result *= characterValue == 0 ? (i+1) : characterValue;
+    }
+    return result;
+  }
+  
+ //max value: (0xC5C1 + 0xC5C1 + 0xC5C1) = 0x25143
+  uuidDecimal += ( multiplyString(s4_1) + multiplyString(s4_2) + multiplyString(s4_3));
+
+  // split s12 in six parts and add the resulting numbers
+  // max: 0xFF + 0x3FC +0xFF0 + 0x3FC0 + 0xFF00 + 0x3FC00 = 0x54FAB
+  const cont = s12.length;
+  for(let i = 0; i < cont; i+=2){
+    // left shifting i times so we can have a position based pow of 2 for this part
+    uuidDecimal += parseInt(s12.substring(i,i+2), 16)<<i;
+  }
+
+  //Max posible value = 0xEE20097FFE
+  return uuidDecimal;
+}
+
+/**
+ * Generate a URL friendly hash from a decimal integer value
+ * @param {numer} value base10 integer number
+ */
+function generateHashFromDecimal(value){
+  // randomized array of characters containing: [0-9],[a-z],[A-Z],[-_]
+  let characters = ['_','J','a','H','Q','1','K','-','0','p','F','h','5','Z','e','W','T','v','X','l','V','9','x',
+          'q','L','c','P','g','z','f','w','6','U','r','B','D','S','R','7','d','C','n','k','b','I','A','j','s','N',
+          'o','t','G','M','O','i','2','8','Y','E','3','y','4','m','u']; 
+  const base = characters.length;
+  
+  let result = "";
+
+  while(value > 0){
+    result += characters[value%base];
+    value = Math.floor(value/base);
+  }
+
+  return result;
+}
 /**
  * Generate a random token that will allow URLs to be (logical) removed
  * @returns {string} uuid v4
@@ -45,10 +119,23 @@ function generateRemoveToken() {
  * @param {string} hash
  * @returns {object}
  */
-async function shorten(url, hash) {
+async function shorten(url) {
 
   if (!isValid(url)) {
-    throw new Error('Invalid URL');
+    throw new Error(`Invalid URL`);
+  }
+
+  //Avoid duplications
+  let previousURL = await getUrl({url:url});
+  
+  if(previousURL != null){
+    return {
+      url,
+      shorten: `${SERVER}/${previousURL.hash}`,
+      hash:previousURL.hash,
+      removeUrl: `${SERVER}/${previousURL.hash}/remove/${previousURL.removeToken}`,
+      created: false
+    };
   }
 
   // Get URL components for metrics sake
@@ -59,6 +146,8 @@ async function shorten(url, hash) {
 
   // Generate a token that will alow an URL to be removed (logical)
   const removeToken = generateRemoveToken();
+  //Generate the hash
+  const hash = generateHash(url);
 
   // Create a new model instance
   const shortUrl = new UrlModel({
@@ -79,7 +168,8 @@ async function shorten(url, hash) {
     url,
     shorten: `${SERVER}/${hash}`,
     hash,
-    removeUrl: `${SERVER}/${hash}/remove/${removeToken}`
+    removeUrl: `${SERVER}/${hash}/remove/${removeToken}`,
+    created: true
   };
 
 }
@@ -98,5 +188,6 @@ module.exports = {
   getUrl,
   generateHash,
   generateRemoveToken,
-  isValid
+  isValid,
+  convertUUIDToDecimal
 }
